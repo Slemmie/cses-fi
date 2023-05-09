a53e17673b630354a299e3d9207f90f61339c95a
Slemmie <thorvejeneriksen@gmail.com>
2023-05-09 02:03:07 +0200

    fix 1728

--- 1728.cpp
+++ 1728.cpp
@@ -2,19 +2,14 @@
 
 int main() {
 	std::ios::sync_with_stdio(0); std::cin.tie(0);
-	
 	int n; std::cin >> n;
 	std::vector <int> a(n);
 	for (int& x : a) std::cin >> x;
-	double ans = 0;
+	__float128 ans = 1e-15;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < i; j++) {
-			ans += double(a[j] > a[i] ? ((a[i] *(a[i] - 1)) >> 1) + (a[j] - a[i]) * a[i] : (a[j] * (a[j] - 1)) >> 1) / double(a[i] * a[j]);
+			ans += a[j] > a[i] ? 1 + (__float128) (a[i] - 1) / (2.0 * a[j]) - (__float128) a[i] / a[j] : (__float128) (a[j] - 1) / (2.0 * a[i]);
 		}
 	}
-	ans *= 1e6;
-	ans = llround(ans);
-	ans /= 1e6;
-	std::cout << std::fixed << std::setprecision(6) << ans << "\n";
-	
+	std::cout << std::fixed << std::setprecision(6) << (long double) ans << "\n";
 }
