d88b49d2c5f70dd55187a25e003cbf20e9f26674
Slemmie <thorvejeneriksen@gmail.com>
2023-05-20 21:30:20 +0200

    1110 fix hack

--- 1110.cpp
+++ 1110.cpp
@@ -2,26 +2,32 @@
 
 int main() {
 	std::ios::sync_with_stdio(0); std::cin.tie(0);
-	
 	std::string s; std::cin >> s;
-	int n = s.length();
-	s += s;
-	int ans = 0, i = 0;
-	while (i < n) {
-		ans = i;
-		int j = i + 1, k = i;
-		while (j < (n << 1) && s[j] >= s[k]) {
-			k += s[j] == s[k];
-			k = s[j] > s[k] ? i : k;
-			j++;
+	int n = s.size();
+	std::vector <int> p(n);
+	std::iota(p.begin(), p.end(), 0);
+	for (int i = 0; i < n; i++) {
+		char m = 'z';
+		for (int x : p) {
+			m = std::min(m, s[(i + x) % n]);
 		}
-		while (i <= k) {
-			i += j - k;
+		std::vector <int> nw;
+		for (int x : p) {
+			if (s[(i + x) % n] == m) {
+				nw.push_back(x);
+			}
 		}
+		p = nw;
+		nw.clear();
+		for (int j = 0; j < (int) p.size(); j++) {
+			if (!j || p[j] - p[j - 1] != i + 1) {
+				nw.push_back(p[j]);
+			}
+		}
+		p = nw;
 	}
-	for (int j = ans; j < ans + n; j++) {
-		std::cout << s[j];
+	for (int i = 0; i < n; i++) {
+		std::cout << s[(i + p[0]) % n];
 	}
 	std::cout << "\n";
-	
 }
