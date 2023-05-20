2c7844d007534700cdd5ba669533b6376cd39b68
Slemmie <thorvejeneriksen@gmail.com>
2023-05-20 20:49:49 +0200

    fix 1139 hack

--- 1139.cpp
+++ 1139.cpp
@@ -1,50 +1,46 @@
 #include <bits/stdc++.h>
- 
-std::vector <std::set <int>*> sub;
-std::vector <std::vector <int>> g;
-std::vector <int> c;
+
 int n;
-std::vector <int> clr;
+std::vector <int> a;
+std::vector <std::vector <int>> g;
 std::vector <int> ans;
- 
-std::set <int>* merge(std::set <std::set<int>*>& st, int node) {
-	if (st.empty()) {
-		clr.push_back(node);
-		return new std::set <int>;
+
+std::set <int> dfs(int node, int par = -1) {
+	std::set <int> st = { a[node] };
+	for (int x : g[node]) {
+		if (x == par) {
+			continue;
+		}
+		auto ch = dfs(x, node);
+		if (ch.size() > st.size()) {
+			std::swap(st, ch);
+		}
+		for (int y : ch) {
+			st.insert(y);
+		}
 	}
-	std::set <int>* r = *prev(st.end());
-	st.erase(prev(st.end()));
-	for (auto& s : st)
-		for (int x : *s)
-			r->insert(x);
-	return r;
+	ans[node] = st.size();
+	return st;
 }
- 
-void dfs(int node = 0, int par = -1) {
-	std::set <std::set <int>*> tmp;
-	for (int x : g[node]) if (x != par) dfs(x, node), tmp.insert(sub[x]);
-	sub[node] = merge(tmp, node);
-	sub[node]->insert(c[node]);
-	ans[node] = sub[node]->size();
-}
- 
+
 int main() {
 	std::ios::sync_with_stdio(0); std::cin.tie(0);
-	
 	std::cin >> n;
-	g.resize(n);
-	sub.resize(n, nullptr);
-	c.resize(n);
-	ans.resize(n);
-	for (int& x : c) std::cin >> x;
-	for (int i = 0; i < n - 1; i++) {
-		int u, v; std::cin >> u >> v; u--, v--;
-		g[u].push_back(v);
-		g[v].push_back(u);
+	a.resize(n);
+	for (int& x : a) {
+		std::cin >> x;
 	}
-	dfs();
-	for (int x : ans) std::cout << x << " ";
+	g.resize(n);
+	for (int i = 0; i < n - 1; i++) {
+		int u, v; std::cin >> u >> v;
+		u--, v--;
+		g[u].emplace_back(v);
+		g[v].emplace_back(u);
+	}
+	ans.resize(n);
+	dfs(0);
+	for (int x : ans) {
+		std::cout << x << " ";
+	}
 	std::cout << "\n";
-	for (int x : clr) delete(sub[x]);
-	
 }
