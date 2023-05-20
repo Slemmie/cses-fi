6013a38b531765d003311813de022271abd17aca
Slemmie <thorvejeneriksen@gmail.com>
2023-05-20 21:19:36 +0200

    fix 1728 hack

--- 1728.cpp
+++ 1728.cpp
@@ -4,12 +4,14 @@ int main() {
 	std::ios::sync_with_stdio(0); std::cin.tie(0);
 	int n; std::cin >> n;
 	std::vector <int> a(n);
-	for (int& x : a) std::cin >> x;
-	__float128 ans = 1e-15;
+	for (int& x : a) {
+		std::cin >> x;
+	}
+	long double ans = 0;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < i; j++) {
-			ans += a[j] > a[i] ? 1 + (__float128) (a[i] - 1) / (2.0 * a[j]) - (__float128) a[i] / a[j] : (__float128) (a[j] - 1) / (2.0 * a[i]);
+		for (int j = i + 1; j < n; j++) {
+			ans += (long double) (a[i] - 1 <= a[j] ? a[i] * (a[i] - 1) / 2 : a[j] * ((a[j] + 1) + (a[i] - a[j] - 1) * 2) / 2) / (a[i] * a[j]);
 		}
 	}
-	std::cout << std::fixed << std::setprecision(6) << (long double) ans << "\n";
+	std::cout << std::fixed << std::setprecision(6) << (double) ans << "\n";
 }
